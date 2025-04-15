cd03c730ac82407a4e140eb2b7431d30e00686ea
h-anand21 <himanshuanand563@gmail.com>
2025-04-15 17:33:44 +0530

    fix issues

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://dainty-heliotrope-95a3b2.netlify.app/",
+    origin: "https://dainty-heliotrope-95a3b2.netlify.app",
   })
 );
 app.use(express.urlencoded({ extended: true }));
